078882480f8afd35b2deea0ebc09a5e7e6b60ee0
salbyul <kpast96@gmail.com>
2023-06-19 18:08:52 +0900

    Initial commit

---
+++ back/src/main/java/com/study/boardvue3/BoardVue3Application.java
@@ -0,0 +1,13 @@
+package com.study.boardvue3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardVue3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardVue3Application.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/study/boardvue3/BoardVue3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.boardvue3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardVue3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
